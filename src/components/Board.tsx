900c4184e26c79a7a84674922ac4d4fb431a426c
moriyama-k <moriyama-k@aidemy.net>
2019-09-11 21:31:27 +0900

    ひっくり返るようにした

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -36,7 +36,7 @@ class Board extends React.Component<IProps, IState> {
   }
 
   // 周り8箇所について、石を置けるか確認する
-  listArountPuttableSquares(stones: number[][], currentStoneNum: number, row: number, column: number): number[][] {
+  listAroundPuttableSquares(stones: number[][], currentStoneNum: number, row: number, column: number): number[][] {
     const puttableSqueares: number[][] = [];
     // 右下チェック
     for (let i = 1; i < Math.min(8 - row, 8 - column); i++) {
@@ -45,7 +45,6 @@ class Board extends React.Component<IProps, IState> {
       if (stones[nextRow][nextColumn] === currentStoneNum) break;
       if (stones[nextRow][nextColumn] === 0) {
         if (i === 1) break;
-        console.log(1);
         puttableSqueares.push([nextRow, nextColumn]);
         break;
       }
@@ -57,7 +56,6 @@ class Board extends React.Component<IProps, IState> {
       if (stones[nextRow][nextColumn] === currentStoneNum) break;
       if (stones[nextRow][nextColumn] === 0) {
         if (i === 1) break;
-        console.log(2);
         puttableSqueares.push([nextRow, nextColumn]);
         break;
       }
@@ -69,7 +67,6 @@ class Board extends React.Component<IProps, IState> {
       if (stones[nextRow][nextColumn] === currentStoneNum) break;
       if (stones[nextRow][nextColumn] === 0) {
         if (i === 1) break;
-        console.log(3);
         puttableSqueares.push([nextRow, nextColumn]);
         break;
       }
@@ -81,7 +78,6 @@ class Board extends React.Component<IProps, IState> {
       if (stones[nextRow][nextColumn] === currentStoneNum) break;
       if (stones[nextRow][nextColumn] === 0) {
         if (i === 1) break;
-        console.log(4);
         puttableSqueares.push([nextRow, nextColumn]);
         break;
       }
@@ -93,7 +89,6 @@ class Board extends React.Component<IProps, IState> {
       if (stones[nextRow][nextColumn] === currentStoneNum) break;
       if (stones[nextRow][nextColumn] === 0) {
         if (i === 1) break;
-        console.log(5);
         puttableSqueares.push([nextRow, nextColumn]);
         break;
       }
@@ -105,7 +100,6 @@ class Board extends React.Component<IProps, IState> {
       if (stones[nextRow][nextColumn] === currentStoneNum) break;
       if (stones[nextRow][nextColumn] === 0) {
         if (i === 1) break;
-        console.log(6);
         puttableSqueares.push([nextRow, nextColumn]);
         break;
       }
@@ -117,7 +111,6 @@ class Board extends React.Component<IProps, IState> {
       if (stones[nextRow][nextColumn] === currentStoneNum) break;
       if (stones[nextRow][nextColumn] === 0) {
         if (i === 1) break;
-        console.log(7);
         puttableSqueares.push([nextRow, nextColumn]);
         break;
       }
@@ -129,40 +122,138 @@ class Board extends React.Component<IProps, IState> {
       if (stones[nextRow][nextColumn] === currentStoneNum) break;
       if (stones[nextRow][nextColumn] === 0) {
         if (i === 1) break;
-        console.log(8);
         puttableSqueares.push([nextRow, nextColumn]);
         break;
       }
     }
-    console.log('puttableSqueares');
-    console.log(puttableSqueares);
     return puttableSqueares;
   }
 
   // 石を返せる場所を割り出し、格納する
   listPuttableSquares(stones: number[][], currentStoneNum: number) {
-    console.log('いくでー')
-    console.time('1')
     let puttableSqueares: number[][] = [];
     stones.forEach((stoneNums, indexRow) => {
       stoneNums.forEach((stoneNum, indexColumn) => {
-        console.log([indexRow, indexColumn]);
         if (currentStoneNum !== stoneNum) return;
-        console.log('お！')
-        const _puttableSqueares = this.listArountPuttableSquares(stones, currentStoneNum, indexRow, indexColumn);
-        console.log(_puttableSqueares);
+        const _puttableSqueares = this.listAroundPuttableSquares(stones, currentStoneNum, indexRow, indexColumn);
         puttableSqueares = puttableSqueares.concat(_puttableSqueares);
-        console.log('現状', puttableSqueares);
       });
     })
-    console.log('最終', puttableSqueares)
     // 重複は取り除く
-    console.timeEnd('1')
     return puttableSqueares.filter((_el: number[], index, arr) => {
       return arr.findIndex((_el2: number[]) => {
         return _el[0] === _el2[0] && _el[1] === _el2[1]
       }) === index;
     })
+  }
+
+  // 周り8箇所について、石をひっくり返す
+  returnAroundPuttableSquares(stones: number[][], currentStoneNum: number, row: number, column: number): number[][] {
+    // 右下チェック
+    for (let i = 1; i < Math.min(8 - row, 8 - column); i++) {
+      let nextRow = row + i;
+      let nextColumn = column + i;
+      if (stones[nextRow][nextColumn] === 0) break;
+      if (stones[nextRow][nextColumn] === currentStoneNum) {
+        if (i === 1) break;
+        for (let j = 0; j < i; j++) {
+          stones[row + j][column + j] = currentStoneNum;
+        }
+        break;
+      }
+    }
+    // 右チェック
+    for (let i = 1; i < 8 - column; i++) {
+      let nextRow = row;
+      let nextColumn = column + i;
+      if (stones[nextRow][nextColumn] === 0) break;
+      if (stones[nextRow][nextColumn] === currentStoneNum) {
+        if (i === 1) break;
+        for (let j = 0; j < i; j++) {
+          stones[row][column + j] = currentStoneNum;
+        }
+        break;
+      }
+    }
+    // 右上チェック
+    for (let i = 1; i < Math.min(row + 1, 8 - column); i++) {
+      let nextRow = row - i;
+      let nextColumn = column + i;
+      if (stones[nextRow][nextColumn] === 0) break;
+      if (stones[nextRow][nextColumn] === currentStoneNum) {
+        if (i === 1) break;
+        for (let j = 0; j < i; j++) {
+          stones[row - j][column + j] = currentStoneNum;
+        }
+        break;
+      }
+    }
+    // 上チェック
+    for (let i = 1; i < row + 1; i++) {
+      let nextRow = row - i;
+      let nextColumn = column;
+      if (stones[nextRow][nextColumn] === 0) break;
+      if (stones[nextRow][nextColumn] === currentStoneNum) {
+        if (i === 1) break;
+        for (let j = 0; j < i; j++) {
+          stones[row - j][column] = currentStoneNum;
+        }
+        break;
+      }
+    }
+    // 左上チェック
+    for (let i = 1; i < Math.min(row + 1, column + 1); i++) {
+      let nextRow = row - i;
+      let nextColumn = column - i;
+      if (stones[nextRow][nextColumn] === 0) break;
+      if (stones[nextRow][nextColumn] === currentStoneNum) {
+        if (i === 1) break;
+        for (let j = 0; j < i; j++) {
+          stones[row - j][column - j] = currentStoneNum;
+        }
+        break;
+      }
+    }
+    // 左チェック
+    for (let i = 1; i < column + 1; i++) {
+      let nextRow = row;
+      let nextColumn = column - i;
+      if (stones[nextRow][nextColumn] === 0) break;
+      if (stones[nextRow][nextColumn] === currentStoneNum) {
+        if (i === 1) break;
+        for (let j = 0; j < i; j++) {
+          stones[row][column - j] = currentStoneNum;
+        }
+        break;
+      }
+    }
+    // 左下チェック
+    for (let i = 1; i < Math.min(8 - row, column + 1); i++) {
+      let nextRow = row + i;
+      let nextColumn = column - i;
+      if (stones[nextRow][nextColumn] === 0) break;
+      if (stones[nextRow][nextColumn] === currentStoneNum) {
+        if (i === 1) break;
+        for (let j = 0; j < i; j++) {
+          stones[row + j][column - j] = currentStoneNum;
+        }
+        break;
+      }
+    }
+    // 下チェック
+    for (let i = 1; i < 8 - row; i++) {
+      let nextRow = row + i;
+      let nextColumn = column;
+      if (stones[nextRow][nextColumn] === 0) break;
+      if (stones[nextRow][nextColumn] === currentStoneNum) {
+        if (i === 1) break;
+        for (let j = 0; j < i; j++) {
+          stones[row + j][column] = currentStoneNum;
+        }
+        break;
+      }
+    }
+    return stones;
   }
 
 
@@ -176,20 +267,8 @@ class Board extends React.Component<IProps, IState> {
       alert('置けるの黒か白です')
       return;
     }
-    let isChanged = false;
-    const stones = (this.state.stones || []).map((stoneNums, indexRow) => {
-      if (indexRow !== row) return stoneNums;
-      return stoneNums.map((_stoneNum, indexColumn) => {
-        if (indexColumn !== column) return _stoneNum;
-        if (_stoneNum !== 0) {
-          alert('すでにおかれています');
-          return _stoneNum
-        }
-        isChanged = true;
-        return currentStoneNum;
-      });
-    });
-    let turn = isChanged ? this.state.turn === 1 ? 2 : 1 : this.state.turn;
+    const stones = this.returnAroundPuttableSquares(this.state.stones, currentStoneNum, row, column);
+    let turn = this.state.turn === 1 ? 2 : 1;
     let puttableSquares = this.listPuttableSquares(stones, turn);
     if (puttableSquares.length === 0) {
       turn = this.state.turn;
